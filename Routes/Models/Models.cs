b95c46b3c22c69f9102e201c6bd0b4e83ae18322
ToT-CaMbIu <36891521+ToT-CaMbIu@users.noreply.github.com>
2019-09-19 19:56:59 +0300

    Initial Commit

---
+++ Routes/Models/Models.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+//using System.Collections.Generic;
+
+namespace Routes.Models
+{
+    public class RouteContext : DbContext
+    {
+        public RouteContext(DbContextOptions<RouteContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+    }
+
+    public class User
+    {
+        public string Nickname { get; set; }
+        public string Country { get; set; }
+        public double Rating { get; set; }
+        public string InfoAbout { get; set; }
+        public bool isPremium { get; set; }
+        public int Id { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class Place
+    {
+        public double Lt { get; set; }
+        public double Lg { get; set; }
+        public string InfoAbout { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class Route
+    {
+        public double Rating { get; set; }
+        public User Creator { get; set; }
+        public int Id { get; set; }
+        public List<Place> Places { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<User> Evaluators { get; set; }
+        public bool isForPremium { get; set; }
+    }
+
+    public class Comment
+    {
+        public User Creator { get; set; }
+        public List<Reply> Replies { get; set; }
+        public string Content { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class Reply
+    {
+        public User Creator { get; set; }
+        public string Content { get; set; }
+        public int Id { get; set; }
+    }
+}
--- Routes/Startup.cs
+++ Routes/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Routes.Models;
 
 namespace Routes
 {
@@ -31,8 +33,11 @@ namespace Routes
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<RouteContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
